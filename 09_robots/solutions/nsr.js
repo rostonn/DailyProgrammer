88ea2a09b30876f950de3800cbbe6dbb4b8e4041
Noah Roston <rostonn@gmail.com>
2015-12-08 19:20:41 -0700

    nsr solution 9

--- 09_robots/solutions/nsr.js
+++ 09_robots/solutions/nsr.js
@@ -1,27 +1,15 @@
 var robot = require('../robots.js');
 
-// console.log(robot);
-
-console.log('Robot One X = '+robot.robotOne['x']);
-console.log('Robot Two X = '+robot.robotTwo['x']);
-
-robot.robotOne.moveOne('left');
-console.log('Robot One X = '+robot.robotOne['x']);
-
-robot.robotOne.x = -10;
+//While loop until one robot is on the marker
 while(!robot.robotOne.onMarker() && !robot.robotTwo.onMarker()){
   robot.robotOne.moveOne('right');
   robot.robotTwo.moveOne('right');
 }
-// console.log(robot.checkCollision());
-// console.log('Robot One X = '+robot.robotOne['x']);
-// console.log('Robot Two X = '+robot.robotTwo['x']);
 
-
+//Move the robot that is not on
 if(robot.robotOne.onMarker()){
   while(!robot.checkCollision()){
     robot.robotTwo.moveOne('left');
-    // console.log('Robot Two X = '+robot.robotTwo['x']);
   }
 }
 else{
