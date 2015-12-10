2ab78010843f43d28b358d05718943fd694f8d57
Noah Roston <rostonn@gmail.com>
2015-12-10 14:06:12 -0700

    nsr 12

--- 12_parensChecker/solutions/nsr.js
+++ 12_parensChecker/solutions/nsr.js
@@ -1,19 +1,30 @@
-function parensChecker(string){
-  var left = 0;
-  var right = 0;
+function parensChecker(s){
+  var string = s.split('');
   for(var i = 0; i < string.length; i++){
-    if(string.charAt(i) == '\('){
-      left += 1;
+    if(string[i] == '('){
+      for(j = i+1; j < string.length; j++){
+        if(string[j] == ')'){
+          string[j] = '';
+          string[i] = '';
+          break;
+        }
+
+        }
+      }
     }
-    else if(string.charAt(i) == '\)'){
-      right += 1;
+    console.log(string);
+    for(var i = 0; i < string.length; i++){
+      if(string[i] == ')' || string[i] == '('){
+        return false;
+      }
     }
-  }
-  return left == right;
+    return true;
 }
 
 var s = '(())';
 var n = '((((()))';
+var k = '()))()(';
+
 
 console.log(parensChecker(s));
-console.log(parensChecker(n));
+console.log(parensChecker("())))("));
